fc29647efba698b6a2c03d83f819a3cffb4c7b0c
Alex Ramsdell <alexramsdell@gmail.com>
2018-01-29 13:43:29 -0800

    wip.

--- storage/index.js
+++ storage/index.js
@@ -1,23 +1,46 @@
-const defaultConfig = {
-    version: '1.0.0',
-    apps: [], // list of config objects for each app
-    pinned: [] // list of appids that are pinned to main page
-};
-
+/*
+ *  EdibleInterfaces Storage Component
+ *
+ *
+ */
 const APP_KEY = 'edible-interfaces';
 
 class Storage {
 
-    constructor() {
+    constructor(appid, appConfig) {
         this.store = window.localStorage || localStorage;
-        const initialized = Boolean(this.store.getItem('edible-interfaces')); 
+        const initialized = Boolean(this.store.getItem(APP_KEY)); 
         if (!initialized) {
-            init();
+            init(appid, appConfig);
         } 
     }
 
-    init() {
-        this.store.setItem('edible-interfaces', JSON.stringify(defaultConfig));
+    init(appid, appConfig) {
+        this.store.setItem(APP_KEY, JSON.stringify(siteConfig));
+    }
+
+    /* get and set data on top level config */ 
+    get() {
+        return JSON.parse(this.store.getItem(APP_KEY));
+    }
+
+    set(data) {
+        this.store.setItem(JSON.stringify(data));
+    }
+
+    /* get and set data by app */
+
+    appGet(appId){
+        const store = this.get();
+        return store.apps[appId];
+    }
+
+    appSet(appId, data) {
+        const store = this.get();
+        store.apps[appId] = data;
+        this.set(store);
     }
 
 }
+
+export default Storage;
