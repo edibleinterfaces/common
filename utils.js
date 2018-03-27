70c08a4469d5559edcaa7585a8be63c6fc132778
Alex Ramsdell <alexramsdell@gmail.com>
2018-03-27 15:08:56 -0700

    add utils file with sassToJs conversion function

---
+++ utils.js
@@ -0,0 +1,12 @@
+export default {
+    cssToThemesObj: (styles) => {
+        return Object.keys(styles).reduce((obj, item) => { 
+            let theme = item.split('-')[0];
+            if (theme in obj)
+                obj[theme].push(styles[item]);
+            else
+                obj[theme] = [styles[item]];
+            return obj;
+        },{});
+    }
+}
